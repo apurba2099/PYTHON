1fc944484b856bb1a8d80b3478a343d2bf6b4cfa
Apurba Dutta <apurbdutta2099@gmail.com>
2023-10-19 00:26:28 +0530

    no change

--- whatsaap.py
+++ whatsaap.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 time.sleep(5)
 count=0
-while count<=90:
-    pyautogui.typewrite("pagli -_-")
+while count<=25:
+    pyautogui.typewrite("Sorry...love you..kal berobo...")
     pyautogui.press("enter")
     count=count+1
