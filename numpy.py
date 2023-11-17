613f365b9e2cf9b63759aef2da6fd0161b8cbbd1
Apurba Dutta <apurbdutta2099@gmail.com>
2023-11-17 12:50:08 +0530

    new

---
+++ numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# create a numpy array with zeros
+my_zeros = np.zeros(5)
+# create a numpy array with ones
+my_ones = np.ones(5)
+# create a numpy array with a range of numbers
+my_range = np.arange(5)
+print(my_zeros)
+print(my_ones)
+print(my_range)
