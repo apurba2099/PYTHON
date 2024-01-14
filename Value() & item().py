189624061cef0dfeac9c9b22c6f28da8d6ae1c29
Apurba Dutta <apurbdutta2099@gmail.com>
2024-01-14 10:42:21 +0530

    exam program

---
+++ Value() & item().py
@@ -0,0 +1,23 @@
+# value()
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+values = my_dict.values()
+print("Values in the dictionary:", values)
+
+# output: Values in the dictionary: dict_values(['John', 25, 'New York'])
+
+
+# item()
+# Example dictionary
+
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+items = my_dict.items()
+print("Items in the dictionary:", items)
+#OutPut:  Items in the dictionary: dict_items([('name', 'John'), ('age', 25), ('city', 'New York')])
+
+
+
+my_array = [12, 45, 78, 34, 56, 89, 23]
+largest_element = max(my_array)
+print(f"The largest element in the array is: {largest_element}")
+#Output: The largest element in the array is: 89
+
